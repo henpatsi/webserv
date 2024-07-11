09d4a257677eb8a6762e1f6b173502fa57acb9f9
Henri Pätsi <patsihenri@gmail.com>
2024-07-11 18:34:55 +0300

    started parsing http request message

--- includes/HttpRequest.hpp
+++ includes/HttpRequest.hpp
@@ -6,7 +6,7 @@
 /*   By: hpatsi <hpatsi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 16:29:51 by hpatsi            #+#    #+#             */
-/*   Updated: 2024/07/11 16:46:54 by hpatsi           ###   ########.fr       */
+/*   Updated: 2024/07/11 18:11:30 by hpatsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,30 @@
 # define HTTPREQUEST_HPP
 
 # include <iostream>
+# include <sstream>
 # include <string>
 # include <map>
 
 # include <unistd.h>
 
-enum request_method
-{
-	GET,
-	POST,
-	DELETE
-};
+# ifndef REQUEST_READ_BUFFER_SIZE
+#  define REQUEST_READ_BUFFER_SIZE 1024
+# endif
 
 class HttpRequest
 {
 	public:
 		HttpRequest( int socketFD );
 		~HttpRequest( void );
+
+		class ReadFailedException : public std::exception
+		{
+			public:
+				const char* what() const throw() { return "read failed"; };
+		};
 	
 	private:
-		enum request_method	method;
+		std::string	method;
 		std::string	resource_path;
 		std::map<std::string, std::string> paramenters;
 };
--- srcs/HttpRequest.cpp
+++ srcs/HttpRequest.cpp
@@ -6,7 +6,7 @@
 /*   By: hpatsi <hpatsi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 16:29:53 by hpatsi            #+#    #+#             */
-/*   Updated: 2024/07/11 16:47:02 by hpatsi           ###   ########.fr       */
+/*   Updated: 2024/07/11 18:34:15 by hpatsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,33 @@
 
 // CONSTRUCTOR
 
+std::string readRequest(int socketFD)
+{
+	std::string requestString;
+	char clientMessageBuffer[REQUEST_READ_BUFFER_SIZE] = {};
+	
+	// TODO check if full message read, read more if not
+	if (read(socketFD, clientMessageBuffer, sizeof(clientMessageBuffer)) == -1)
+		throw HttpRequest::ReadFailedException();
+
+	requestString += clientMessageBuffer;
+	return requestString;
+}
+
 HttpRequest::HttpRequest(int socketFD)
 {
-	char clientMessageBuffer[1024] = {};
-	if (read(socketFD, clientMessageBuffer, sizeof(clientMessageBuffer)) == -1)
-	{
-		std::cout << "Reading client message failed";
-		return ;
-	}
-	std::cout << clientMessageBuffer << "\n";
+	std::string requestString = readRequest(socketFD);
+	std::cout << requestString << "\n";
+
+	std::istringstream sstream(requestString);
+	std::string line;
+
+	sstream >> this->method;
+	std::cout << "Method: " << this->method << "\n";
+	
+	sstream >> line;
+	this->resource_path = line.substr(0, line.find('?'));
+	std::cout << "Resource path: " << this->resource_path << "\n";
 }
 
 // DESTRUCTOR
