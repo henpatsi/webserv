ddeb793d643505561315426b5e44987e085412bd
Henri Pätsi <patsihenri@gmail.com>
2024-07-16 13:25:25 +0300

    some more early detections of bad requests

--- srcs/HttpRequest.cpp
+++ srcs/HttpRequest.cpp
@@ -6,7 +6,7 @@
 /*   By: hpatsi <hpatsi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 16:29:53 by hpatsi            #+#    #+#             */
-/*   Updated: 2024/07/16 11:43:31 by hpatsi           ###   ########.fr       */
+/*   Updated: 2024/07/16 13:12:33 by hpatsi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void extractParameters(std::map<std::string, std::string>& parametersMap, std::s
 			std::string value = parameter.substr(parameter.find('=') + 1, std::string::npos);
 			parametersMap[key] = value;
 		}
-		if (parametersString.find('&') == std::string::npos)
+		if (parametersString.find('&') == std::string::npos || parametersString.find('&') == parametersString.size() - 1)
 			break ;
 		parametersString = parametersString.substr(parametersString.find('&') + 1, std::string::npos);
 	}
@@ -38,6 +38,11 @@ HttpRequest::HttpRequest(std::string requestMessageString)
 
 	// Assumes the first word in the request is the method
 	sstream >> this->method;
+	if (this->method.empty())
+	{
+		badRequest = true;
+		return ;
+	}
 
 	// Assumes the second word in the request is the url
 	std::string url;
@@ -55,6 +60,11 @@ HttpRequest::HttpRequest(std::string requestMessageString)
 
 	// Assumes the third word in the request is the http version
 	sstream >> this->httpVersion;
+	if (this->httpVersion.empty())
+	{
+		badRequest = true;
+		return ;
+	}
 
 	std::string line;
 	std::getline(sstream, line); // Reads the rest of the first line
@@ -73,6 +83,12 @@ HttpRequest::HttpRequest(std::string requestMessageString)
 		}
 		std::string key = line.substr(0, line.find(':'));
 		std::string value = line.substr(line.find(':') + 2, line.find('\r'));
+		if (key.empty() || value.empty()) // TODO also check that not just spaces if necessary
+		{
+			//std::cout << "Invalid header: " << line << "\n";
+			badRequest = true;
+			return ;
+		}
 		this->headers[key] = value;
 	}
 
@@ -91,10 +107,10 @@ HttpRequest::HttpRequest(std::string requestMessageString)
 	std::cout << "HTTP version: " << this->httpVersion << "\n";
 	std::cout << "Url Parameters:\n";
 	for (std::map<std::string, std::string>::iterator it = this->urlParameters.begin(); it != this->urlParameters.end(); it++)
-		std::cout << it->first << " = " << it->second << "\n";
+		std::cout << "  " << it->first << " = " << it->second << "\n";
 	std::cout << "Headers:\n";
 	for (std::map<std::string, std::string>::iterator it = this->headers.begin(); it != this->headers.end(); it++)
-		std::cout << it->first << " = " << it->second << "\n";
+		std::cout << "  " << it->first << " = " << it->second << "\n";
 	std::cout << "Content:\n " << this->content << "\n";
 	std::cout << "REQUEST INFO FINISHED\n\n";
 }
