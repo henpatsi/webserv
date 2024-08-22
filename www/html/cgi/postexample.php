2dbb093e75475302e3eb85a505d838b02300638a
Taavi Oramo <taavioramo@gmail.com>
2024-08-22 11:22:56 +0300

    update postexample test

--- www/html/cgi/postexample.php
+++ www/html/cgi/postexample.php
@@ -4,7 +4,9 @@ content-type: text/html
 <body>
 
 Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
-
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Environment variables: <?php var_dump(getenv());?><br>
+      From stdin: <?php echo file_get_contents('php://stdin');?><br>
+      From php://input: <?php echo file_get_contents('php://input');?><br>
 </body>
 </html>
