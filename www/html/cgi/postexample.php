4c3c75261c357957e0b98143559a685230122814
Henri Patsi <hpatsi@student.hive.fi>
2024-08-26 14:30:19 +0300

    removed website files

--- www/html/cgi/postexample.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?><br>
-Environment variables: <?php var_dump(getenv());?><br>
-From php://input: <?php echo file_get_contents('php://input');?><br>
-</body>
-</html>
--- www/html/cgi/printstdin.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-print("content-type:text/html")
-print("")
-input_string = sys.stdin.readlines()
-print(input_string)
